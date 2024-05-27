5f5664f661b9ce2c9a0bb03bb7b7c5d5426918b2
CPisGod <90697999+CPisGod@users.noreply.github.com>
2024-05-28 00:16:50 +0900

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,18 +1,16 @@
-import math
-
-a = input(" >> ")
+a = input("수를 입력하세요 ( 예 : 6 + 8 ) > ")
 b = a.split(" ")
 b[0], b[2] = float(b[0]), float(b[2])
 sign = ['+','-','*','/','^']
 
 if b[1] in sign:
     if b[1] == "+":
-        print(b[0] + b[2])
+        print(f'결과 : {b[0]} + {b[2]} = {b[0] + b[2]}')
     elif b[1] == "-":
-        print(b[0] - b[2])
+        print(f'결과 : {b[0]} - {b[2]} = {b[0] - b[2]}')
     elif b[1] == "*":
-        print(b[0] * b[2])
+        print(f'결과 : {b[0]} * {b[2]} = {b[0] * b[2]}')
     elif b[1] == "/":
-        print(b[0] / b[2])
+        print(f'결과 : {b[0]} / {b[2]} = {b[0] / b[2]}')
     elif b[1] == "^":
-        print(b[0] ** b[2])
+        print(f'결과 : {b[0]} ^ {b[2]} = {b[0] ** b[2]}')
